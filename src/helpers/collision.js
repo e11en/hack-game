a153ace388572a2ffedeb24d37f091315dc773fe
e.langelaar <e.langelaar@tedas.nl>
2021-02-04 20:05:00 +0100

    Started with interaction

--- src/helpers/collision.js
+++ src/helpers/collision.js
@@ -1,15 +1,15 @@
 import { SetColliding } from 'state/actions';
 import { Direction, MovementSpeed } from "./constants";
 
-const singleHitTest = (collingWith, x, y, width, height) =>
+const singleHitTest = (collingWith, x, y, hitTester) =>
 {
     let collidingObject;
     collingWith.filter(obj => obj.enabled).forEach(obj => {
         const hitBoxWidth = obj.hitBox ? obj.hitBox.width : obj.width;
         const hitBoxHeight = obj.hitBox ? obj.hitBox.height : obj.height;
 
-        if (x < obj.x + hitBoxWidth  && x + width  > obj.x &&
-            y < obj.y + hitBoxHeight && y + height > obj.y) {
+        if (x < obj.x + hitBoxWidth  && x + hitTester.width  > obj.x &&
+            y < obj.y + hitBoxHeight && y + hitTester.height > obj.y) {
                 collidingObject = obj;
                 return;
         }
@@ -21,36 +21,38 @@ const singleHitTest = (collingWith, x, y, width, height) =>
 const hasCollision = (isColliding, collisionObject, collidingDirection, isCollidingState, collidingWithState, dispatch) => {
     if(isColliding !== isCollidingState || collidingWithState !== collisionObject) {
         dispatch(SetColliding(isColliding, collisionObject, collidingDirection));
+
+        if (collisionObject && collisionObject.hasInteraction)
+        {
+            console.log("CAN INTERACT");
+        }
     }
 }
 
-export const hitTest = (characterRef, mapObjectsContext, isCollidingState, collidingWithState, x, y, width, height, dispatch) => {
-    if (!characterRef || !characterRef.current)
-        return;
-
-    const collidingLeft = singleHitTest(mapObjectsContext, x - MovementSpeed, y, width, height);
+export const hitTest = (collisionObjects, isCurrentlyColliding, currentCollidingWith, hitTester, dispatch) => {
+    const collidingLeft = singleHitTest(collisionObjects, hitTester.x - MovementSpeed, hitTester.y, hitTester);
     if (collidingLeft) {
-        hasCollision(true, collidingLeft, Direction.LEFT, isCollidingState, collidingWithState, dispatch);
+        hasCollision(true, collidingLeft, Direction.LEFT, isCurrentlyColliding, currentCollidingWith, dispatch);
         return;
     }
 
-    const collidingRight = singleHitTest(mapObjectsContext, x + MovementSpeed, y, width, height);
+    const collidingRight = singleHitTest(collisionObjects, hitTester.x + MovementSpeed, hitTester.y, hitTester);
     if (collidingRight) {
-        hasCollision(true, collidingRight, Direction.RIGHT, isCollidingState, collidingWithState, dispatch);
+        hasCollision(true, collidingRight, Direction.RIGHT, isCurrentlyColliding, currentCollidingWith, dispatch);
         return;
     }
 
-    const collidingUp = singleHitTest(mapObjectsContext, x, y - MovementSpeed, width, height);
+    const collidingUp = singleHitTest(collisionObjects, hitTester.x, hitTester.y - MovementSpeed, hitTester);
     if (collidingUp) {
-        hasCollision(true, collidingUp, Direction.UP, isCollidingState, collidingWithState, dispatch);
+        hasCollision(true, collidingUp, Direction.UP, isCurrentlyColliding, currentCollidingWith, dispatch);
         return;
     }
 
-    const collidingDown = singleHitTest(mapObjectsContext, x, y + MovementSpeed, width, height);
+    const collidingDown = singleHitTest(collisionObjects, hitTester.x, hitTester.y + MovementSpeed, hitTester);
     if (collidingDown) {
-        hasCollision(true, collidingDown, Direction.DOWN, isCollidingState, collidingWithState, dispatch);
+        hasCollision(true, collidingDown, Direction.DOWN, isCurrentlyColliding, currentCollidingWith, dispatch);
         return;
     }
 
-    hasCollision(false, null, null, isCollidingState, collidingWithState, dispatch);
+    hasCollision(false, null, null, isCurrentlyColliding, currentCollidingWith, dispatch);
 };
