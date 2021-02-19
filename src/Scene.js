dab04c6222b5b86e715d0d7f91d5f9b7d468f5a3
e.langelaar <e.langelaar@tedas.nl>
2021-02-19 14:41:43 +0100

    trying to fix colliding

--- src/Scene.js
+++ src/Scene.js
@@ -21,7 +21,10 @@ export default class Scene extends Phaser.Scene {
         const backgroundImage = this.add.image(0, 0, 'background').setOrigin(0, 0);
         backgroundImage.setScale(2);
 
-        //const console = new Console(this, 100, 300);
+        // const console = this.physics.add.existing(new Console(this, 100, 300));
+        // this.physics.arcade.enableBody(console);
+        // this.objects = this.physics.add.staticGroup();
+        // this.objects.create(100, 300, 'console');
 
         const consoleObject = this.physics.add.staticImage(100, 300, 'console').setScale(2).refreshBody();
 
@@ -47,6 +50,20 @@ export default class Scene extends Phaser.Scene {
             frames: this.anims.generateFrameNumbers('player', { frames: [9,10,11]}),
             frameRate: 10
         });
+
+        
+        this.physics.add.collider(
+            this.player,
+            consoleObject,
+            function (_player, _console)
+            {
+                if (_player.body.touching.up && _console.body.touching.down)
+                {
+                    console.log("Colliding");             
+                }
+            });
+
+        this.physics.add.overlap(this.player, consoleObject, null, null, this);
     }
 
     update() {
